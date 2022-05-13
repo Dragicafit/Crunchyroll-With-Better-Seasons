cdf12539f4c2363ed172a2be315b6147c46a7ee8
Damien HERBERT <dherbert98@gmail.com>
2022-05-13 22:45:06 +0200

    test improveApiSeasons type

---
+++ __tests__/web-accessible-resources/tabImproveApiSeasons.test.ts
@@ -0,0 +1,19 @@
+import { improveApiSeasons } from "../../src/web-accessible-resources/tabImproveApiSeasons";
+
+it("test improveApiSeasons type", () => {
+  for (const [key, value] of improveApiSeasons) {
+    expect(typeof key).toEqual("string");
+    expect(
+      ["SUB", "EN", "FR", "ES", "PT", "DE", "RU", "OTHERS"].includes(value.lang)
+    ).toBeTruthy();
+    expect(
+      value.season_number == null ||
+        typeof value.season_number === "string" ||
+        typeof value.season_number === "number"
+    ).toBeTruthy();
+    expect(
+      value.season_number_order == null ||
+        typeof value.season_number_order === "number"
+    ).toBeTruthy();
+  }
+});
