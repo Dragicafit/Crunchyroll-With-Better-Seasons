7128cbd44d386b380d076a1446e780fcb34b25d7
Damien HERBERT <dherbert98@gmail.com>
2024-03-13 00:53:56 +0100

    remove old regex for sub and dub episode

--- src/service/subtitleService.ts
+++ src/service/subtitleService.ts
@@ -1,6 +1,5 @@
 import {
   localeToDisplay,
-  mapping,
   subtitleLocales,
   subtitleLocalesWithSUB,
   supported,
@@ -8,12 +7,8 @@ import {
 } from "../web-accessible-resources/tabConst";
 
 const regexSupported = /(JSON\.parse\(')(\["\w{2}(?:-\w+){0,3}",[-\w",]*?fr-FR[-\w",]*?\])('\))/;
-const regexSupportedFallbacks = /(JSON\.parse\(')(\{"\w{2}(?:-\w+){0,3}":\["\w{2}(?:-\w+){0,3}"[-\w",\[\]:]*"\]\})('\))/;
 const regexSupportedFallbacks2 =
   /(JSON\.parse\(')(\{"default":\["\w{2}(?:-\w+){0,3}"\],"\w{2}(?:-\w+){0,3}":\[[-\w",:\[\]]*?"\]\})('\))/;
-const regexMapping = /(JSON\.parse\(')(\{"Wp":\{"\w+":\{"to":"\w{2}(?:-\w+){0,3}","desc":".*?"\}\}\})('\))/;
-const regexLocaleToDisplay =
-  /(JSON\.parse\(')(\{"\w{2}(?:-\w+){0,3}":"[^{}]*?,"":""\})('\))/;
 const regexLocaleToDisplay2 =
   /(\d+:e=>\{"use strict";e\.exports=JSON\.parse\(')(\{"en-US":".*?"\})('\)\})/;
 
@@ -22,38 +17,12 @@ export default class SubtitleService {
     const supported: supported = JSON.parse(
       body.match(regexSupported)![2].replaceAll("\\'", "'")
     );
-    const supportedFallbacks: supportedFallbacks = JSON.parse(
-      body.match(regexSupportedFallbacks)![2].replaceAll("\\'", "'")
-    );
     const supportedFallbacks2: supportedFallbacks = JSON.parse(
       body.match(regexSupportedFallbacks2)![2].replaceAll("\\'", "'")
-    );
-    const mapping: mapping = JSON.parse(
-      body.match(regexMapping)![2].replaceAll("\\'", "'")
-    );
-    const localeToDisplay: localeToDisplay = JSON.parse(
-      body.match(regexLocaleToDisplay)![2].replaceAll("\\'", "'")
     );
     const localeToDisplay2: localeToDisplay = JSON.parse(
       body.match(regexLocaleToDisplay2)![2].replaceAll("\\'", "'")
     );
-
-    for (const subtitleLocale of [
-      ...(<subtitleLocales[]>Object.keys(supportedFallbacks)),
-    ]) {
-      const subtitleLocaleWithSUB: subtitleLocalesWithSUB = <
-        subtitleLocalesWithSUB
-      >(subtitleLocale + "SUB");
-      const display: string =
-        localeToDisplay[subtitleLocale.toLowerCase()] + " (Sub)";
-      supported.push(<any>subtitleLocaleWithSUB);
-      supportedFallbacks[subtitleLocaleWithSUB] = [subtitleLocale, "en-US"];
-      mapping.Wp[subtitleLocaleWithSUB] = {
-        to: <any>subtitleLocaleWithSUB,
-        desc: display,
-      };
-      localeToDisplay[subtitleLocaleWithSUB.toLowerCase()] = display;
-    }
 
     for (const subtitleLocale of [
       ...(<subtitleLocales[]>Object.keys(localeToDisplay2)),
@@ -72,20 +41,8 @@ export default class SubtitleService {
         `$1${JSON.stringify(supported).replaceAll("'", "\\'")}$3`
       )
       .replace(
-        regexSupportedFallbacks,
-        `$1${JSON.stringify(supportedFallbacks).replaceAll("'", "\\'")}$3`
-      )
-      .replace(
         regexSupportedFallbacks2,
         `$1${JSON.stringify(supportedFallbacks2).replaceAll("'", "\\'")}$3`
-      )
-      .replace(
-        regexMapping,
-        `$1${JSON.stringify(mapping).replaceAll("'", "\\'")}$3`
-      )
-      .replace(
-        regexLocaleToDisplay,
-        `$1${JSON.stringify(localeToDisplay).replaceAll("'", "\\'")}$3`
       )
       .replaceAll(
         new RegExp(regexLocaleToDisplay2, "g"),
